43bb479d4a2c7649385a8c867b18e339d62816e2
Sushmitha-94 <st.sushmi@gmail.com>
2021-09-26 13:55:38 -0500

    DLL CLL

--- src/CircularDLinkedList.java
+++ src/CircularDLinkedList.java
@@ -1,7 +1,6 @@
 public class CircularDLinkedList {
 
-	Node head =null;
-	Node tail = null;
+	static Node head = null;
 	
 	public static class Node{
 		int data;
@@ -14,17 +13,99 @@ public class CircularDLinkedList {
 	}
 	
 	//insert new node
+	public static void insertFirst(Node node) {
+		if(head==null) {
+			head = node;
+			head.previous = node;
+			head.next = node;
+		}
+		else {
+			node.next = head;
+			node.previous = head.previous;
+			head.previous =node;
+			node.previous.next = node;
+			head = node;
+			
+		}
+	}
 	
 	//insert at position
+	public static void insertAt(Node node, int pos) {
+		Node currNode = head;
+		if(pos==1) {
+			insertFirst(node);
+		}
+		else {
+			currNode = currNode.next;
+			for(int i=2; i<pos; i++) {
+				if(currNode!=null)
+					currNode = currNode.next;
+			}
+			
+			currNode.previous.next = node;
+			node.previous = currNode.previous;
+			node.next = currNode;
+			currNode.previous = node;
+		}
+	}
 	
 	//print linked list
+	public static void printll() {
+		Node node =head;
+		do{
+			System.out.println(node.data);
+			node = node.next;
+		}while(node!=head);
+		
+	}
+	
+	// print backwards
+	public static void printBacK() {
+		Node node = head;
+		while(node.next!=null) {
+			node = node.next;
+		}
+		while(node!=head) {
+			System.out.println(node.data);
+			node = node.previous;
+		}
+	}
+	
 	
 	//delete by key
+	public static void deleteKey(int key) {
+		Node node=head;
+		while(node!=null) {
+			if(node.data==key) {
+				node.previous.next = node.next;
+				node.next.previous = node.previous;
+				if(node==head)
+					head = node.next;
+				break;
+			}
+			node = node.next;
+		}
+	}
 	
 	// 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		insertFirst(new Node(10));
+		insertFirst(new Node(8));
+		insertFirst(new Node(7));
+		insertFirst(new Node(5));
+		insertFirst(new Node(3));
+		insertAt(new Node(9),5);
+		insertAt(new Node(2),1);
+		printll();
+		System.out.println("After 6 inserted:");
+		insertAt(new Node(6),4);
+		printll();
+		deleteKey(7);
+		System.out.println("Delete 7:");
+		printll();
+		//printBacK();
 	}
 
 }
--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -1,8 +1,6 @@
-
 public class DoublyLinkedList {
 
-	Node head =null;
-	Node tail = null;
+static Node head =null;
 	
 	public static class Node{
 		int data;
@@ -15,17 +13,96 @@ public class DoublyLinkedList {
 	}
 	
 	//insert new node
+	public static void insertFirst(Node node) {
+		if(head==null) {
+			head = node;
+			head.previous = null;
+			head.next = null;
+		}
+		else {
+			node.next = head;
+			node.previous = null;
+			head.previous =node;
+			head = node;
+		}
+	}
 	
 	//insert at position
+	public static void insertAt(Node node, int pos) {
+		Node currNode = head;
+		if(pos==1) {
+			insertFirst(node);
+		}
+		else {
+			currNode = currNode.next;
+			for(int i=2; i<pos; i++) {
+				if(currNode!=null)
+					currNode = currNode.next;
+			}
+			
+			currNode.previous.next = node;
+			node.previous = currNode.previous;
+			node.next = currNode;
+			currNode.previous = node;
+		}
+	}
 	
 	//print linked list
+	public static void printll() {
+		Node node =head;
+		while(node!=null) {
+			System.out.println(node.data);
+			node = node.next;
+		}
+		
+	}
+	
+	// print backwards
+	public static void printBacK() {
+		Node node = head;
+		while(node.next!=null) {
+			node = node.next;
+		}
+		while(node!=null) {
+			System.out.println(node.data);
+			node = node.previous;
+		}
+	}
+	
 	
 	//delete by key
+	public static void deleteKey(int key) {
+		Node node=head;
+		while(node!=null) {
+			if(node.data==key) {
+				node.previous.next = node.next;
+				node.next.previous = node.previous;
+				if(node==head)
+					head = node.next;
+				break;
+			}
+			node = node.next;
+		}
+	}
 	
 	// 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		insertFirst(new Node(10));
+		insertFirst(new Node(8));
+		insertFirst(new Node(7));
+		insertFirst(new Node(5));
+		insertFirst(new Node(3));
+		insertAt(new Node(9),5);
+		insertAt(new Node(2),1);
+		insertAt(new Node(6),4);
+		printll();
+		System.out.println("Backwards:");
+		printBacK();
+		System.out.println("Delete 7");
+		deleteKey(7);
+		printll();
 	}
 
 }
